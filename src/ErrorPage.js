ab9fc9c2e1a0a6184e196ef75f700b102cbfc671
Anish Gupta <anish.gupta05497@gmail.com>
2023-07-18 22:16:40 +0530

    Added FAQ in help

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+  const err = useRouteError();
   return (
-    <div>
-      Error
+    <div className="container text-center">
+      <h1 className="font-black text-3xl">Oops!!</h1>
+      <h2 className="text-2xl">Something went wrong</h2>
+      <h2 className="text-lg">{err.status + ": " + err.statusText}</h2>
     </div>
-  )
-}
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
--- src/component/Help.js
+++ src/component/Help.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FAQ } from "../utils/constant";
+import { SlArrowUp, SlArrowDown } from "react-icons/sl";
 
-const Help = () => {
+const Section = ({id, title, description }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible((prevVisible)=> !prevVisible)
+  }
+
   return (
-    <div className='container'>
-      Help
+    <div className="flex flex-col shadow rounded-md p-2.5 m-2.5 cursor-pointer" onClick={toggleVisibility}>
+      <div className="flex justify-between items-center"> 
+        <h3 className="font-semibold text-lg text-title">{title}</h3>
+        {
+        isVisible ? (
+        <SlArrowUp onClick={toggleVisibility} className="cursor-pointer" />
+      ) : (
+        <SlArrowDown onClick={toggleVisibility} className="cursor-pointer" />
+      )}
+      </div>
+      {isVisible && <p className="text-bio text-base">{description}</p>}
     </div>
   )
-}
+};
 
-export default Help
+const Help = () => {
+  const [visibleSection, setVisibleSection] = useState("");
+
+  return (
+    <div className="container">
+      <div className="card-conatiner">
+        <h1 className="card-container-title pb-5">FAQ</h1>
+        {FAQ.map((question) => {
+          return (
+            <Section
+              key={question.id}
+              id={question.id}
+              title={question.title}
+              description={question.description}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Help;
